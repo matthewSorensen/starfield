d6346a4c218483eb23b25ab416f28c8a930f546a
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2023-01-22 16:50:20 -0800

    Make that a lot faster with the hhg thingy. Generating the points now takes most of the time - but just generating at the finest layer takes 0.75 of the time, so there is less room for improvment.

--- Starfield.py
+++ Starfield.py
@@ -12,16 +12,12 @@ import matplotlib.tri as tri
 from numpy.random import rand
 import math
 from collections import defaultdict
-import heapq
 matplotlib.rcParams['figure.figsize'] = [14.0,14.0]
-import triangle
 import inspect
 import ezdxf
-from scipy.spatial import KDTree
 from dataclasses import dataclass
 import time
-#from cvxopt import spmatrix, solvers, matrix
-#from hhg import HHG
+from hhg import HHG
 
 
 # In[2]:
@@ -125,66 +121,49 @@ def inside_box(dimension, radius):
 # In[4]:
 
 
-dimension = 25 # Overall size that we should fill with the pattern
-trace = 0.003 * 25.4 # Minimum feature size we can fabricate
-space = 0.003 * 25.4 # Minimum distance between features we can fabricate
-fraction = 0.25      # Scale between poisson disc spacing and circle radius
-keepout = 1.1
-starting_feature = 1 # The radius of the largest disc in the pattern
-exponent = 0.75 # Each time we iterate, how much do we shrink everything?
-
-points = [np.array((0.5 * dimension,0.5 * dimension))]
-scale, generation, generations =  starting_feature, 0, {0: {0}}
-scales = [scale]
-
-while True:
-    # Calculate the new feature size, check if it's too small to make
-    scale *= exponent
-    if 2 * scale < trace:
-        break
-    scales.append(scale)
-    # Compute the new points
-    old_count = len(points)
-    pdsample(dimension, dimension, scale / fraction, points, criteria = inside_box(dimension, scale))
-    # Record them in generation log
-    generation += 1
-    generations[generation] =set(range(old_count, len(points)))
-
-points = np.array(points)
-scales = np.array(scales)
+get_ipython().run_cell_magic('time', '', "dimension = 25 # Overall size that we should fill with the pattern\ntrace = 0.003 * 25.4 # Minimum feature size we can fabricate\nspace = 0.003 * 25.4 # Minimum distance between features we can fabricate\nfraction = 0.25      # Scale between poisson disc spacing and circle radius\nkeepout = 1.1\nstarting_feature = 1 # The radius of the largest disc in the pattern\nexponent = 0.75 # Each time we iterate, how much do we shrink everything?\n\npoints = [np.array((0.5 * dimension,0.5 * dimension))]\nscale, generation, generations =  starting_feature, 0, {0: {0}}\nscales = [scale]\n\nwhile True:\n    # Calculate the new feature size, check if it's too small to make\n    scale *= exponent\n    if 2 * scale < trace:\n        break\n    scales.append(scale)\n    # Compute the new points\n    old_count = len(points)\n    pdsample(dimension, dimension, scale / fraction, points, criteria = inside_box(dimension, scale))\n    # Record them in generation log\n    generation += 1\n    generations[generation] =set(range(old_count, len(points)))\n\npoints = np.array(points)\nscales = np.array(scales)")
 
 
-# In[5]:
+# In[ ]:
 
 
-for i in range(len(generations) - 1):
-    # Build a k-d of this generation...
-    idx = list(generations[i])
-    tree = KDTree(points[idx])
-    #...then filter all the other generations with it with a given radius.
-    effective_radius = max(keepout * scales[i], space + scales[i])
-    
-    for j in range(i+1, len(generations)):
-        generations[j] = {k for k in generations[j] if tree.query_ball_point(points[k],effective_radius + scales[j]) == []}
 
 
-# In[6]:
+
+# In[ ]:
 
 
-for _,g in generations.items():
-    l = list(g)
-    plt.scatter(points[l,0],points[l,1])
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[7]:
 
 
+get_ipython().run_cell_magic('time', '', '                \ndef candidates(old, new, dead):\n    for a,b in old.check_upwards(new):\n        if b in dead:\n            continue\n        yield a,b\n    for b,a in new.check_upwards(old,skip_identical = True):\n        if b in dead:\n            continue\n        yield a,b\n                \ndef check_collision(points,prev_radii, generation, radius):\n    def check(old, new):\n        delta = points[old] - points[new]\n        return delta.dot(delta) < (prev_radii[generation[old]] + radius)**2\n    return check\n\ngeneration = dict()\neffective_radii = np.maximum(keepout * scales, space + scales)\nbounding_box = np.min(points, axis = 0) - max(effective_radii), np.max(points, axis = 0) + max(effective_radii)\n#Build an empty hierarchical hash grid that we will then merge everything into.\nprev = HHG(bounding_box)\n\nfor i in range(len(generations)):\n    # Index this generation\n    new = HHG(bounding_box)\n    r = effective_radii[i]\n    for j in generations[i]:\n        new.insert_bbox(points[j] - r, points[j] + r, j)\n    # Then filter this generation against the previous ones\n    dead = set()\n    check = check_collision(points, effective_radii, generation, scales[i])\n\n    for old_element, new_element in candidates(prev,new,dead):\n        if check(old_element, new_element):\n            dead.add(new_element)\n    # Add this back in, and then set the generation for the surviving points\n    prev.merge(new, filter = lambda x: x not in dead)\n    for p in generations[i]:\n        if p not in dead:\n            generation[p] = i')
+
+
+# In[ ]:
+
+
+
+
+
+# In[8]:
+
+
 doc = ezdxf.new(dxfversion="R2010")
 msp = doc.modelspace()
 
-for i,v in generations.items():
-    for k in v:
-        msp.add_circle((points[k,0],points[k,1]), radius = scales[i])
+for _,grid in prev.grids.items():
+    for _,v in grid.items():
+        for k in v:
+            msp.add_circle((points[k,0],points[k,1]), radius = scales[generation[k]])
         
 doc.saveas("test_small.dxf")
 
@@ -201,7 +180,7 @@ doc.saveas("test_small.dxf")
 
 
 
-# In[8]:
+# In[ ]:
 
 
 @dataclass
@@ -217,17 +196,20 @@ def format_circle(center, radius, error, layer):
     yield ['width', 0]
 
 
-# In[9]:
+# In[ ]:
 
 
 layer = 'F.SilkS'
 module_name = 'starfield-calibrator-front'
 circles = []
 n = 0
+        
+for _,grid in prev.grids.items():
+    for _,v in grid.items():
+        for k in v:
+            circles.append(format_circle(points[k],scales[generation[k]], 0.001, layer))
 
-for i,v in generations.items():
-    for k in v:
-        circles.append(format_circle(points[k],scales[i], 0.001, layer))
+        
         
 
 def write_sexp(sexp,fp):
@@ -258,4 +240,10 @@ expr = header + circles
     
 with open(module_name + ".kicad_mod", "w") as f:
     write_sexp(expr,f)
+
+
+# In[ ]:
+
+
+
 
---
+++ hhg.py
@@ -0,0 +1,136 @@
+from collections import defaultdict
+import math
+import numpy as np
+import pybloomfilter
+
+def floor_log2(n):
+    lg = 0
+    while n > 1:
+        lg += 1
+        n //= 2
+    return lg
+
+
+class HHG:
+    
+    @staticmethod
+    def calculate_box_position(low, high):
+    
+        span = max(high - low)
+        level = floor_log2(int(math.floor(1/span)))
+        space = 1 / (2**level)
+        lo = np.floor(low / space).astype(int)
+        hi = np.floor(high / space).astype(int)
+    
+        delta = sum(hi - lo)
+    
+        while delta != 0:
+            level -= 1
+            lo //= 2
+            hi //= 2
+        
+            delta = sum(hi - lo)
+    
+        return level, lo[0], lo[1] 
+    
+    def __init__(self, box = None, reverse_index_scale = None):
+        self.grids = defaultdict(lambda: defaultdict(set))
+        self.boxes = dict()
+        self.filter = pybloomfilter.BloomFilter(2**reverse_index_scale, 0.01) if reverse_index_scale else None
+        # And store the optional scaling box if one other than [0,1]^2 is provided
+        self.low = box[0] if box else None
+        self.span = box[1] - box[0] if box else None
+             
+    def insert_bbox(self,low,high,i):
+        # If we're working in a transformed coordinate space, transform the bounding box
+        if self.low is not None:
+            low = (low - self.low) / self.span
+            high = (high - self.low) / self.span
+        
+        level,ix,iy = HHG.calculate_box_position(low,high)
+        self.grids[level][(ix,iy)].add(i)
+        self.boxes[i] = (level,ix,iy)
+        
+        while level > -1:
+            if self.filter:
+                self.filter.update([(level,ix,iy)])
+            level -= 1
+            ix //= 2
+            iy //= 2
+                
+            if level in self.grids and (ix,iy) in self.grids[level]:
+                break          
+    
+    def intersection_sets(self, dead):
+        for level in sorted(self.grids.keys(), key = lambda x: 0 -x):
+            grid = self.grids[level]
+            for idx, items in grid.items():
+                if len(items) != 1:
+                    yield items, items
+                else:
+                    m, = items
+                    if m in dead:
+                        continue
+                    
+                ix,iy = idx
+                for j in range(level-1,-1,-1):
+                    ix //= 2
+                    iy //= 2
+                    
+                    if j not in self.grids:
+                        continue
+                    grid = self.grids[j]
+
+                    if (ix,iy) not in grid:
+                        continue
+                    other = grid[(ix,iy)]
+                    
+                    if len(other) == 1:
+                        m, = other
+                        if m in dead:
+                            continue
+                    yield items, other
+                    
+    def intersections(self, dead):
+        for x,y in self.intersection_sets(dead):
+            for a in x:
+                for b in y:
+                    if (x is y and b <= a) or (a in dead) or (b in dead):
+                        continue
+                    yield a,b
+
+
+    def check_upwards(self, other, skip_identical = False):
+        """ Check the elements of this HHG against the equal-to-larger elements
+        of another HHG. If skip_identical is provided, only check strictly larger ones.
+
+        Yields a list of possible intersection candidates, with the first element being
+        from this grid, and the second from the other grid. """
+        for i,g in self.grids.items():
+            for (idx,idy),v in g.items():
+                level = i
+                if skip_identical:
+                    idx //= 2
+                    idy //= 2
+                    level -= 1
+                for _ in range(i+1):
+                    if level in other.grids and (idx,idy) in other.grids[level]:
+                        for a in other.grids[level][(idx,idy)]:
+                            for b in v:
+                                yield b,a
+                    idx //= 2
+                    idy //= 2
+                    level -= 1
+
+    def merge(self, other, filter = None):
+        """ Takes another HHG (must have an identical bounding box - not checked!), and adds all of its elements
+        to this one. If a filter function is provided, it first checks them with it and only adds passing elements. """
+
+        for level, grid in other.grids.items():
+            for idx, elements in grid.items():
+                for v in elements:
+                    if filter is not None and not filter(v):
+                        continue
+                    
+                    self.grids[level][idx].add(v)
+
